ca8cbd83a7fb7898283e9698b63808ac7e6449c9
tenzin <ten13zin@gmail.com>
2018-11-06 10:55:42 +0530

    change to f-string

--- formatter.py
+++ formatter.py
@@ -1,7 +1,6 @@
 import argparse
 import re
 from pathlib import Path
-Path('C:\Program Files').parent
 
 POS = [
     'ADJ', 'ADP', 'ADV', 'AUX', 'CCONJ', 'DET', 'INTJ', 'NOUN', 'NUM',
@@ -13,7 +12,7 @@ find_pos = lambda line: [pos for pos in POS if pos in line]
 def rdrformat(in_fn):
 
     # Extract word/tag
-    word_tags = []
+    word_tags = ''
     with open(in_fn, 'r') as f:
         count = 0
         for line in f.readlines():
@@ -21,12 +20,12 @@ def rdrformat(in_fn):
             if match:
                 tg = find_pos(line)
                 tg = 'X' if not tg else tg[0]
-                word_tags.append('{}/{}'.format(match[0].rstrip(), tg))
+                word_tags += f'{match[0].rstrip()}/{tg} '
 
     # save the dataset
     data_dir = Path(in_fn).parent
     with open(data_dir/'goldTrain', 'w') as f:
-        f.write(' '.join(word_tags))
+        f.write(word_tags)
 
 
 parser = argparse.ArgumentParser()
